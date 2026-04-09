8f41be5e8a2897d64873e5a38921f31da28b6988
Đoàn Hữu Toàn <25521869@gm.uit.edu.vn>
2026-04-09 19:04:08 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,29 @@
-
+#include <iostream>
+#include "PhanSo.h"
+using namespace std;
+int main(){
+    PhanSo a, b;
+    cout << "Nhap phan so A:" << endl;
+    a.Nhap();
+    cout << "Nhap phan so B:" << endl;
+    b.Nhap();
+    a.RutGon();
+    b.RutGon();
+    cout << "Phan so A (rut gon): "; a.Xuat(); cout << endl;
+    cout << "Phan so B (rut gon): "; b.Xuat(); cout << endl;
+    PhanSo tong = a.Tong(b);
+    cout << "Tong (A + B)   : "; tong.Xuat(); cout << endl;
+    PhanSo hieu = a.Hieu(b);
+    cout << "Hieu (A - B)   : "; hieu.Xuat(); cout << endl;
+    PhanSo tich = a.Tich(b);
+    cout << "Tich (A * B)   : "; tich.Xuat(); cout << endl;
+    PhanSo thuong = a.Thuong(b);
+    cout << "Thuong (A / B) : "; thuong.Xuat(); cout << endl;
+    // So sánh
+    cout << "So sanh A va B : ";
+    int cmp = a.SoSanh(b);
+    if (cmp == 1) cout << "A > B";
+    else if (cmp == -1) cout << "A < B";
+    else cout << "A = B";
+    return 0;
+}
