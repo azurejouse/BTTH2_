bff2a717f4b89401c6f3344830ce29d064c7b942
Đoàn Hữu Toàn <25521869@gm.uit.edu.vn>
2026-04-09 20:09:33 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
-#include "SoPhuc.h"
+#include "GioPhutGiay.h"
 using namespace std;
 int main(){
-    SoPhuc sp1, sp2;
-    cout << "Nhap so phuc A:\n"; sp1.Nhap();
-    cout << "Nhap so phuc B:\n"; sp2.Nhap();
-    cout << "\nSo phuc A: "; sp1.Xuat();
-    cout << "\nSo phuc B: "; sp2.Xuat();
-    cout << "\nA + B = "; sp1.Tong(sp2).Xuat();
-    cout << "\nA - B = "; sp1.Hieu(sp2).Xuat();
-    cout << "\nA * B = "; sp1.Tich(sp2).Xuat();
-    cout << "\nA / B = "; sp1.Thuong(sp2).Xuat();
+    GioPhutGiay t;
+    cout << "Nhap thoi gian: \n";
+    t.Nhap();
+    cout << "\nThoi gian vua nhap: ";
+    t.Xuat();
+    t.TinhCongThemMotGiay();
+    cout << "\nThoi gian sau khi cong 1 giay: ";
+    t.Xuat();
     cout << endl;
     return 0;
 }
