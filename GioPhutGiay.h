915b8b682efbe9b5bed315723d74eb83297ba7b0
Đoàn Hữu Toàn <25521869@gm.uit.edu.vn>
2026-04-09 20:08:51 +0700

    Update and rename SoPhuc.h to GioPhutGiay.h

---
+++ GioPhutGiay.h
@@ -0,0 +1,11 @@
+#ifndef GIOPHUTGIAY_H
+#define GIOPHUTGIAY_H
+class GioPhutGiay {
+private:
+    int iGio, iPhut, iGiay; // Thuộc tính giờ, phút, giây
+public:
+    void Nhap();
+    void Xuat();
+    void TinhCongThemMotGiay();
+};
+#endif
